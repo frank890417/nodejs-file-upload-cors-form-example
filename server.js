da0658fa9adca24b1ddd9dbf93d5e7c899ea2f14
Che-Yu Wu <frank890417@gmail.com>
2022-12-09 14:54:58 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require("express");
+var cors = require('cors')
+const multer = require("multer");
+const upload = multer();
+
+const corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200
+};
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors())
+app.post("/upload_files", upload.array("files"), uploadFiles);
+function uploadFiles(req, res, next) {
+    // console.log(req)
+    console.log(req.body);
+    res.send("File uploaded successfully.");
+}
+app.listen(5001, () => {
+    console.log(`Server started...`);
+});
